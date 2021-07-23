1040d1a1eb092b7a61f12aa1e35556af117a07ab
EC2 Default User <ec2-user@ip-172-31-65-164.ec2.internal>
2021-07-23 12:57:10 +0000

    aws-project-v2-dynamodb-read

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,10 +1,12 @@
 #!flask/bin/python
 import json
-from flask import Flask, Response
+from flask import Flask, Response, request
 from helloworld.flaskrun import flaskrun
 
 import requests
 from flask_cors import CORS
+import boto3
+
 
 
 application = Flask(__name__)
@@ -19,6 +21,20 @@ def get():
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    
+    
+    
+@application.route('/get_customers', methods=['GET'])
+def get_customers():
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('customers')
+    # replace table scan
+    resp = table.scan()
+    print(str(resp))
+    return Response(json.dumps(str(resp['Items'][0]['salary'])), mimetype='application/json', status=200)
+ 
+
+
 
 if __name__ == '__main__':
     flaskrun(application)
