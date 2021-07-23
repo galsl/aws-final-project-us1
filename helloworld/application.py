cc2535d899a4f30dcc21ddbe68d37dc782d58bd8
EC2 Default User <ec2-user@ip-172-31-65-164.ec2.internal>
2021-07-23 16:00:50 +0000

    aws-project-dynamodb-read-V4

--- helloworld/application.py
+++ helloworld/application.py
@@ -6,6 +6,7 @@ from helloworld.flaskrun import flaskrun
 import requests
 from flask_cors import CORS
 import boto3
+import uuid
 
 
 
@@ -26,15 +27,30 @@ def post():
     
 @application.route('/get_customers', methods=['GET'])
 def get_customers():
+    data = request.get_json()
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     table = dynamodb.Table('customers')
     # replace table scan
     resp = table.scan()
     print(str(resp))
     return Response(json.dumps(str(resp['Items'])), mimetype='application/json', status=200)
-
-
-
+    #curl -i http://"localhost:8000/get_customers"
+    
+    
+@application.route('/add_customer', methods=['POST'])
+def add_customer():
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('customers')
+    data = request.data
+    data_dict = json.loads(data)
+    customer_uuid = (str(uuid.uuid4()))
+    data_dict['id2'] = customer_uuid
+    print(id)
+    table.put_item(Item=data_dict)
+    
+    
+    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+        
 
 if __name__ == '__main__':
     flaskrun(application)
